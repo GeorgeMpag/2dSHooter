287047dee09c07038785e4242b1de604b79251c4
GeorgeMpag <geormpag@gmail.com>
2021-09-15 14:27:02 +0300

    remove player ending frame

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ function handleAnimations(){
    
     if (65 in keys || 68 in keys|| 87 in keys|| 83 in keys){
       
-        if (ia==19){
+        if (ia==18){
             ia =0
         }      
 
